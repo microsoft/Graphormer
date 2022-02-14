a5db766335e2d08f25dce35b8c7375caac8188ca
Yu Shi <shiyu_k1994@qq.com>
2022-02-14 12:22:13 +0000

    update run.sh

--- examples/property_prediction/run.sh
+++ examples/property_prediction/run.sh
@@ -2,9 +2,12 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-mkdir checkpoint_$1_$2_$3_$4
+git_commit=$(git rev-parse HEAD)
+echo "Using git commit ${git_commit}"
+checkpoint_dir=checkpoint_$1_$2_$3_$4_${git_commit}
+mkdir checkpoint_$1_$2_$3_$4_${git_commit}
 
-if [[$4 == "fp16"]]; then
+if [[ $4 == "fp16" ]]; then
     if [[ $3 == "graphormer_v2" ]] && [[ $1 == "large" ]] && [[ $2 == "pcqm4mv2" ]]; then
         fairseq-train \
         --user-dir ../../graphormer \
@@ -24,7 +27,7 @@ if [[$4 == "fp16"]]; then
         --fp16 \
         --data-buffer-size 20 \
         --max-epoch 300 \
-        --save-dir /blob/mol/checkpoint_$1_$2_$3
+        --save-dir /blob/mol/${checkpoint_dir}
     elif [[ $3 == "graphormer_v2" ]] && [[ $1 == "large" ]]; then
         fairseq-train \
         --user-dir ../../graphormer \
@@ -44,7 +47,27 @@ if [[$4 == "fp16"]]; then
         --fp16 \
         --data-buffer-size 20 \
         --max-epoch 300 \
-        --save-dir /blob/mol/checkpoint_$1_$2_$3
+        --save-dir /blob/mol/${checkpoint_dir}
+    elif [[ $3 == "graphormer_v2" ]]; then
+        fairseq-train \
+        --user-dir ../../graphormer \
+        --num-workers 16 \
+        --ddp-backend=legacy_ddp \
+        --dataset-name $2 \
+        --dataset-source ogb \
+        --task graph_prediction \
+        --criterion l1_loss \
+        --arch graphormer_$1 \
+        --num-classes 1 \
+        --attention-dropout 0.1 --act-dropout 0.1 --dropout 0.0 \
+        --optimizer adam --adam-betas '(0.9, 0.999)' --adam-eps 1e-8 --clip-norm 5.0 --weight-decay 0.0 \
+        --lr-scheduler polynomial_decay --power 1 --warmup-updates 60000 --total-num-update 1000000 \
+        --lr 2e-4 --end-learning-rate 1e-9 \
+        --batch-size 64 \
+        --fp16 \
+        --data-buffer-size 20 \
+        --max-epoch 300 \
+        --save-dir /blob/mol/${checkpoint_dir}
     elif [[ $3 == "graphormer_v1" ]]; then
         fairseq-train \
         --user-dir ../../graphormer \
@@ -64,7 +87,7 @@ if [[$4 == "fp16"]]; then
         --fp16 \
         --data-buffer-size 20 \
         --max-epoch 300 \
-        --save-dir /blob/mol/checkpoint_$1_$2_$3
+        --save-dir /blob/mol/${checkpoint_dir}
     fi
 else
     if [[ $3 == "graphormer_v2" ]] && [[ $1 == "large" ]] && [[ $2 == "pcqm4mv2" ]]; then
@@ -85,7 +108,7 @@ else
         --batch-size 64 \
         --data-buffer-size 20 \
         --max-epoch 300 \
-        --save-dir /blob/mol/checkpoint_$1_$2_$3
+        --save-dir /blob/mol/${checkpoint_dir}
     elif [[ $3 == "graphormer_v2" ]] && [[ $1 == "large" ]]; then
         fairseq-train \
         --user-dir ../../graphormer \
@@ -104,7 +127,26 @@ else
         --batch-size 64 \
         --data-buffer-size 20 \
         --max-epoch 300 \
-        --save-dir /blob/mol/checkpoint_$1_$2_$3
+        --save-dir /blob/mol/${checkpoint_dir}
+    elif [[ $3 == "graphormer_v2" ]]; then
+        fairseq-train \
+        --user-dir ../../graphormer \
+        --num-workers 16 \
+        --ddp-backend=legacy_ddp \
+        --dataset-name $2 \
+        --dataset-source ogb \
+        --task graph_prediction \
+        --criterion l1_loss \
+        --arch graphormer_$1 \
+        --num-classes 1 \
+        --attention-dropout 0.1 --act-dropout 0.1 --dropout 0.0 \
+        --optimizer adam --adam-betas '(0.9, 0.999)' --adam-eps 1e-8 --clip-norm 5.0 --weight-decay 0.0 \
+        --lr-scheduler polynomial_decay --power 1 --warmup-updates 60000 --total-num-update 1000000 \
+        --lr 2e-4 --end-learning-rate 1e-9 \
+        --batch-size 64 \
+        --data-buffer-size 20 \
+        --max-epoch 300 \
+        --save-dir /blob/mol/${checkpoint_dir}
     elif [[ $3 == "graphormer_v1" ]]; then
         fairseq-train \
         --user-dir ../../graphormer \
@@ -123,5 +165,6 @@ else
         --batch-size 64 \
         --data-buffer-size 20 \
         --max-epoch 300 \
-        --save-dir /blob/mol/checkpoint_$1_$2_$3
+        --save-dir /blob/mol/${checkpoint_dir}
     fi
+fi
