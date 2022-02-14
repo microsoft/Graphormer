dbe302cc44910ed14918b837bf8b4ffa68e82342
Yu Shi <shiyu_k1994@qq.com>
2022-02-14 12:09:00 +0000

    add fp16 option in run.sh

--- examples/property_prediction/run.sh
+++ examples/property_prediction/run.sh
@@ -2,66 +2,126 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-mkdir checkpoint_$1_$2_$3
+mkdir checkpoint_$1_$2_$3_$4
 
-if [[ $3 == "graphormer_v2" ]] && [[ $1 == "large" ]] && [[ $2 == "pcqm4mv2" ]]; then
-    fairseq-train \
-    --user-dir ../../graphormer \
-    --num-workers 16 \
-    --ddp-backend=legacy_ddp \
-    --dataset-name $2 \
-    --dataset-source ogb \
-    --task graph_prediction \
-    --criterion l1_loss \
-    --arch graphormer_$1 \
-    --num-classes 1 \
-    --attention-dropout 0.1 --act-dropout 0.1 --dropout 0.0 \
-    --optimizer adam --adam-betas '(0.9, 0.999)' --adam-eps 1e-8 --clip-norm 5.0 --weight-decay 0.0 \
-    --lr-scheduler polynomial_decay --power 1 --warmup-updates 60000 --total-num-update 1000000 \
-    --lr 4e-5 --end-learning-rate 1e-9 \
-    --batch-size 64 \
-    --fp16 \
-    --data-buffer-size 20 \
-    --max-epoch 300 \
-    --save-dir /blob/mol/checkpoint_$1_$2_$3
-elif [[ $3 == "graphormer_v2" ]] && [[ $1 == "large" ]]; then
-    fairseq-train \
-    --user-dir ../../graphormer \
-    --num-workers 16 \
-    --ddp-backend=legacy_ddp \
-    --dataset-name $2 \
-    --dataset-source ogb \
-    --task graph_prediction \
-    --criterion l1_loss \
-    --arch graphormer_$1 \
-    --num-classes 1 \
-    --attention-dropout 0.1 --act-dropout 0.1 --dropout 0.0 \
-    --optimizer adam --adam-betas '(0.9, 0.999)' --adam-eps 1e-8 --clip-norm 5.0 --weight-decay 0.0 \
-    --lr-scheduler polynomial_decay --power 1 --warmup-updates 60000 --total-num-update 1000000 \
-    --lr 8e-5 --end-learning-rate 1e-9 \
-    --batch-size 64 \
-    --fp16 \
-    --data-buffer-size 20 \
-    --max-epoch 300 \
-    --save-dir /blob/mol/checkpoint_$1_$2_$3
-elif [[ $3 == "graphormer_v1" ]]; then
-    fairseq-train \
-    --user-dir ../../graphormer \
-    --num-workers 16 \
-    --ddp-backend=legacy_ddp \
-    --dataset-name $2 \
-    --dataset-source ogb \
-    --task graph_prediction \
-    --criterion l1_loss \
-    --arch graphormer_$1_v1 \
-    --num-classes 1 \
-    --attention-dropout 0.1 --input-dropout 0.0 --dropout 0.1 \
-    --optimizer adam --adam-betas '(0.9, 0.999)' --adam-eps 1e-8 --clip-norm 5.0 --weight-decay 0.0 \
-    --lr-scheduler polynomial_decay --power 1 --warmup-updates 60000 --total-num-update 1000000 \
-    --lr 2e-4 --end-learning-rate 1e-9 \
-    --batch-size 64 \
-    --fp16 \
-    --data-buffer-size 20 \
-    --max-epoch 300 \
-    --save-dir /blob/mol/checkpoint_$1_$2_$3
-fi
+if [[$4 == "fp16"]]; then
+    if [[ $3 == "graphormer_v2" ]] && [[ $1 == "large" ]] && [[ $2 == "pcqm4mv2" ]]; then
+        fairseq-train \
+        --user-dir ../../graphormer \
+        --num-workers 16 \
+        --ddp-backend=legacy_ddp \
+        --dataset-name $2 \
+        --dataset-source ogb \
+        --task graph_prediction \
+        --criterion l1_loss \
+        --arch graphormer_$1 \
+        --num-classes 1 \
+        --attention-dropout 0.1 --act-dropout 0.1 --dropout 0.0 \
+        --optimizer adam --adam-betas '(0.9, 0.999)' --adam-eps 1e-8 --clip-norm 5.0 --weight-decay 0.0 \
+        --lr-scheduler polynomial_decay --power 1 --warmup-updates 60000 --total-num-update 1000000 \
+        --lr 4e-5 --end-learning-rate 1e-9 \
+        --batch-size 64 \
+        --fp16 \
+        --data-buffer-size 20 \
+        --max-epoch 300 \
+        --save-dir /blob/mol/checkpoint_$1_$2_$3
+    elif [[ $3 == "graphormer_v2" ]] && [[ $1 == "large" ]]; then
+        fairseq-train \
+        --user-dir ../../graphormer \
+        --num-workers 16 \
+        --ddp-backend=legacy_ddp \
+        --dataset-name $2 \
+        --dataset-source ogb \
+        --task graph_prediction \
+        --criterion l1_loss \
+        --arch graphormer_$1 \
+        --num-classes 1 \
+        --attention-dropout 0.1 --act-dropout 0.1 --dropout 0.0 \
+        --optimizer adam --adam-betas '(0.9, 0.999)' --adam-eps 1e-8 --clip-norm 5.0 --weight-decay 0.0 \
+        --lr-scheduler polynomial_decay --power 1 --warmup-updates 60000 --total-num-update 1000000 \
+        --lr 8e-5 --end-learning-rate 1e-9 \
+        --batch-size 64 \
+        --fp16 \
+        --data-buffer-size 20 \
+        --max-epoch 300 \
+        --save-dir /blob/mol/checkpoint_$1_$2_$3
+    elif [[ $3 == "graphormer_v1" ]]; then
+        fairseq-train \
+        --user-dir ../../graphormer \
+        --num-workers 16 \
+        --ddp-backend=legacy_ddp \
+        --dataset-name $2 \
+        --dataset-source ogb \
+        --task graph_prediction \
+        --criterion l1_loss \
+        --arch graphormer_$1_v1 \
+        --num-classes 1 \
+        --attention-dropout 0.1 --input-dropout 0.0 --dropout 0.1 \
+        --optimizer adam --adam-betas '(0.9, 0.999)' --adam-eps 1e-8 --clip-norm 5.0 --weight-decay 0.0 \
+        --lr-scheduler polynomial_decay --power 1 --warmup-updates 60000 --total-num-update 1000000 \
+        --lr 2e-4 --end-learning-rate 1e-9 \
+        --batch-size 64 \
+        --fp16 \
+        --data-buffer-size 20 \
+        --max-epoch 300 \
+        --save-dir /blob/mol/checkpoint_$1_$2_$3
+    fi
+else
+    if [[ $3 == "graphormer_v2" ]] && [[ $1 == "large" ]] && [[ $2 == "pcqm4mv2" ]]; then
+        fairseq-train \
+        --user-dir ../../graphormer \
+        --num-workers 16 \
+        --ddp-backend=legacy_ddp \
+        --dataset-name $2 \
+        --dataset-source ogb \
+        --task graph_prediction \
+        --criterion l1_loss \
+        --arch graphormer_$1 \
+        --num-classes 1 \
+        --attention-dropout 0.1 --act-dropout 0.1 --dropout 0.0 \
+        --optimizer adam --adam-betas '(0.9, 0.999)' --adam-eps 1e-8 --clip-norm 5.0 --weight-decay 0.0 \
+        --lr-scheduler polynomial_decay --power 1 --warmup-updates 60000 --total-num-update 1000000 \
+        --lr 4e-5 --end-learning-rate 1e-9 \
+        --batch-size 64 \
+        --data-buffer-size 20 \
+        --max-epoch 300 \
+        --save-dir /blob/mol/checkpoint_$1_$2_$3
+    elif [[ $3 == "graphormer_v2" ]] && [[ $1 == "large" ]]; then
+        fairseq-train \
+        --user-dir ../../graphormer \
+        --num-workers 16 \
+        --ddp-backend=legacy_ddp \
+        --dataset-name $2 \
+        --dataset-source ogb \
+        --task graph_prediction \
+        --criterion l1_loss \
+        --arch graphormer_$1 \
+        --num-classes 1 \
+        --attention-dropout 0.1 --act-dropout 0.1 --dropout 0.0 \
+        --optimizer adam --adam-betas '(0.9, 0.999)' --adam-eps 1e-8 --clip-norm 5.0 --weight-decay 0.0 \
+        --lr-scheduler polynomial_decay --power 1 --warmup-updates 60000 --total-num-update 1000000 \
+        --lr 8e-5 --end-learning-rate 1e-9 \
+        --batch-size 64 \
+        --data-buffer-size 20 \
+        --max-epoch 300 \
+        --save-dir /blob/mol/checkpoint_$1_$2_$3
+    elif [[ $3 == "graphormer_v1" ]]; then
+        fairseq-train \
+        --user-dir ../../graphormer \
+        --num-workers 16 \
+        --ddp-backend=legacy_ddp \
+        --dataset-name $2 \
+        --dataset-source ogb \
+        --task graph_prediction \
+        --criterion l1_loss \
+        --arch graphormer_$1_v1 \
+        --num-classes 1 \
+        --attention-dropout 0.1 --input-dropout 0.0 --dropout 0.1 \
+        --optimizer adam --adam-betas '(0.9, 0.999)' --adam-eps 1e-8 --clip-norm 5.0 --weight-decay 0.0 \
+        --lr-scheduler polynomial_decay --power 1 --warmup-updates 60000 --total-num-update 1000000 \
+        --lr 2e-4 --end-learning-rate 1e-9 \
+        --batch-size 64 \
+        --data-buffer-size 20 \
+        --max-epoch 300 \
+        --save-dir /blob/mol/checkpoint_$1_$2_$3
+    fi
